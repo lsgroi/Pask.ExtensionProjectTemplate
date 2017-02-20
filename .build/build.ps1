96b76429a24725d3ba655f28343a0449afbd1545
Luca Sgroi <lucasgroi@gmail.com>
2017-02-20 22:35:01 +0000

    Add build steps

--- .build/build.ps1
+++ .build/build.ps1
@@ -2,4 +2,13 @@ Import-Task Restore-NuGetPackages, Clean, Build
 Set-Property BuildConfiguration -Value Release
 
 # Synopsis: Default task
-Task . Restore-NuGetPackages, Clean, Build
+Task . Restore-NuGetPackages, Clean, Build, Copy-VSIX
+
+# Synopsis: Copy the Visual Studio Extension Installer to the build output directory
+Task Copy-VSIX {
+    New-Directory $BuildOutputFullPath | Out-Null
+    Copy-Item -Path (Join-Path $SolutionFullPath "Pask.ExtensionProjectTemplateWizard\bin\x86\Release\Pask.ExtensionProjectTemplateWizard.vsix") -Destination $BuildOutputFullPath
+}
+
+# Synopsis: Release task
+Task Release Restore-NuGetPackages, Clean, Build, Copy-VSIX
