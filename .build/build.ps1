0a82933c000190397cd799ecfebd15cdb2162994
Luca Sgroi <lucasgroi@gmail.com>
2017-04-17 10:37:47 +0100

    Add build log messages

--- .build/build.ps1
+++ .build/build.ps1
@@ -5,7 +5,7 @@ Set-Property ReleaseAssetPattern -Value "Pask.ExtensionProjectTemplate"
 Import-Task Restore-NuGetPackages, Clean, Build, Version-Assemblies, Version-BuildServer, New-GitHubRelease
 
 Set-Property BuildConfiguration -Value Release
-Set-Property Version -Value (Get-SemanticVersion "0.12.2")
+Set-Property Version -Value (Get-SemanticVersion "0.13.0")
 Set-Property ExcludeAssemblyInfo  -Value @("Pask.ExtensionProjectTemplate\AssemblyInfo.cs")
 
 # Synopsis: Default task
@@ -17,19 +17,26 @@ Task Release Version-BuildServer, Restore-NuGetPackages, Clean, Version-Assembli
 # Synopsis: Copy the Visual Studio Extension Installer to the build output directory
 Task Copy-VSIX {
     New-Directory $BuildOutputFullPath | Out-Null
+    $Source = Join-Path $SolutionFullPath "Pask.ExtensionProjectTemplateWizard\bin\x86\Release\Pask.ExtensionProjectTemplateWizard.vsix"
+    $Destination = $BuildOutputFullPath
+    "Copying {0} to $Destination" -f (Split-Path $Source -Leaf)
     Copy-Item -Path (Join-Path $SolutionFullPath "Pask.ExtensionProjectTemplateWizard\bin\x86\Release\Pask.ExtensionProjectTemplateWizard.vsix") -Destination $BuildOutputFullPath
 }
 
 # Synopsis: Apply the current version to the VSIX manifest
 Task Version-VSIXManifest {
     $VSIXManifestFile = Join-Path $SolutionFullPath "Pask.ExtensionProjectTemplateWizard\source.extension.vsixmanifest"
+    "Applying {0} to {1}" -f $Version.SemVer, (Split-Path $VSIXManifestFile -Leaf)
     [xml]$VSIXManifest = Get-Content $VSIXManifestFile
     $VSIXManifest.PackageManifest.Metadata.Identity.Version = $Version.SemVer
     $VSIXManifest.Save($VSIXManifestFile)
+
     $VSTemplateFile = Join-Path $SolutionFullPath "Pask.ExtensionProjectTemplate\Pask.ExtensionProjectTemplate.vstemplate"
+    $AssemblyVersion = "{0}.0.0.0" -f $Version.Major
+    "Applying {0} to {1}" -f $AssemblyVersion, (Split-Path $VSTemplateFile -Leaf)
     [xml]$VSTemplate = Get-Content $VSTemplateFile
     $Assembly = $VSTemplate.VSTemplate.WizardExtension.Assembly -split ", "
-    $Assembly[1] = "Version=$($Version.Major).0.0.0"
+    $Assembly[1] = "Version={0}" -f $AssemblyVersion
     $VSTemplate.VSTemplate.WizardExtension.Assembly = $Assembly -join ", "
     $VSTemplate.Save($VSTemplateFile)
 }
